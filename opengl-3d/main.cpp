55d6678653e97a5ee97d1d4d7ca001453f6b3593
Leo <le4ndro.fioretti@gmail.com>
2025-02-07 00:11:21 +0100

    boh

--- opengl-3d/main.cpp
+++ opengl-3d/main.cpp
@@ -6,6 +6,12 @@
 constexpr auto WINDOW_WIDTH = 800;
 constexpr auto WINDOW_HEIGHT = 600;
 
+// Resize handler
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -15,8 +21,7 @@ int main() {
 
 	// Init GLFW
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "3D test", NULL, NULL);
-	if (window == NULL)
-	{
+	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -24,13 +29,26 @@ int main() {
 	glfwMakeContextCurrent(window);
 
 	// Init GLAD
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+	// Handle resize
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Swap color buffer
+		glfwSwapBuffers(window);
+
+		// Handle window state change (like keyboard press)
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
